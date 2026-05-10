8de525a118a0227a040d4c3e12363c48ea2b8b3f
Bhavana <kamisettybhavana9@gmail.com>
2026-05-11 02:56:34 +0530

    commiting from working directory to local repo without staging area

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 This is config file
 This is new line
+This is 3rd line to verify git commit -am "commit msg" command
--- menu.sh
+++ menu.sh
@@ -1,2 +1,3 @@
 This is my menu file.
 This is my new line
+This is 3rd line to verify git commit -am "commit msg" command 
